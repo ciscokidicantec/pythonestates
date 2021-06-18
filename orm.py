6aa4eeef55fd278c4d540d784361c66a14aab96d
mario wakeham <mariowakeham@outlook.com>
2021-06-18 13:38:30 +0100

    got dropping table working but uses sql

--- orm.py
+++ orm.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from datetime import datetime
 
 from flask import Flask,redirect,url_for,render_template,request
 from sqlalchemy import create_engine, engine
@@ -7,10 +8,10 @@ from sqlalchemy.ext.declarative.api import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy import Column,ForeignKey,Integer,String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+#from sqlalchemy import Column,ForeignKey,Integer,String
+#from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.orm import relationship
+#from sqlalchemy import create_engine
 
 app=Flask(__name__)
 
@@ -27,6 +28,9 @@ class Vendors(db.Model):
     vendor_type = db.Column(db.String(255),nullable=False)
     vendor_company_name =db.Column(db.String(255),nullable=False)
     vendor_contact_name = db.Column(db.String(255),nullable=False)
+    vendor_onmarketdate = db.Column(db.DateTime,nullable=False,default=datetime.now) #utcnow
+
+
 
  #   def __init__(self,vendor_id,vendor_active,vendor_type,vendor_company_name,vendor_contact_name):
   #      self.vendor_id = vendor_id
@@ -48,9 +52,19 @@ class Vendors(db.Model):
        # print(f"\nVendor_Company Name {self.vendor_company_name}")
         #print(f"\nVendor Contect Name {self.vendor_contact_name}")
 
-
 @app.route("/")
+def home():
+
+
+   return "At The Home Page"
+
+
+@app.route("/v")
 def create_vendors():
+
+   sql = 'DROP TABLE IF EXISTS Vendors;'
+   engine = create_engine(os.getenv('DATABASE_URL'))
+   result = engine.execute(sql)
 
    db.create_all()
    v1=Vendors(vendor_active="y",vendor_type="e", vendor_company_name="Tiger Estates", vendor_contact_name="Sam Houston")
@@ -65,7 +79,7 @@ def create_vendors():
   #  v1.print_vendors()
     #v2.print_vendors()
 
-   return f'The vendor type is {vendor.vendor_type}'
+   return f'The vendor type is {vendor.vendor_type} First Came On Market {vendor.vendor_onmarketdate}'
 
 
 
