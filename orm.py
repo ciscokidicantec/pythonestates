ccf5ece0de22a33079f273388642cc59c119b490
mario wakeham <mariowakeham@outlook.com>
2021-06-23 07:55:41 +0100

    completed login form

--- orm.py
+++ orm.py
@@ -79,6 +79,10 @@ class Vendors(db.Model):
        # print(f"\nVendor_Company Name {self.vendor_company_name}")
         #print(f"\nVendor Contect Name {self.vendor_contact_name}")
 
+@app.route("/about", methods =  ["POST","GET"])
+def about():
+   return render_template("about.html", title='About')        
+
 
 @app.route("/register", methods = ["POST","GET"])
 def register():
@@ -88,9 +92,15 @@ def register():
       return redirect(url_for('home'))
    return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods = ["POST","GET"])
 def login():
    form = LoginForm()
+   if form.validate_on_submit():
+      if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+         flash('You have been logged in sucessfully!','success')
+         return redirect(url_for('home'))
+      else:
+         flash('Log in unsuccessful, please check user name and password','danger')
    return render_template('login.html', title='Login', form=form)
 
 
